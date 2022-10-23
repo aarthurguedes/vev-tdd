829bee6a0419cfe14f24b547e09f1e818941fe2a
felipedaf <me@felipedaf.dev>
2022-10-22 23:25:39 -0400

    Adds the method register_bank_slip in a Bank_Slip_Manager

--- src/bank_slip_manager/app/bank_slip_manager.py
+++ src/bank_slip_manager/app/bank_slip_manager.py
@@ -1,5 +1,12 @@
+from payment import Payment
+
 class Bank_Slip_Manager:
     def __init__(self, invoice):
         self.invoice = invoice
         self.total_paid = 0
+
+    def register_bank_slip(self, bank_slip):
+        new_payment = Payment(bank_slip.date, bank_slip.value, "BOLETO")
+
+        self.invoice.add_payment(new_payment)
 
--- src/bank_slip_manager/tests/bank_slip_manager_test.py
+++ src/bank_slip_manager/tests/bank_slip_manager_test.py
@@ -4,6 +4,8 @@ sys.path.insert(1, "../app")
 import unittest
 from invoice import Invoice
 from bank_slip_manager import Bank_Slip_Manager
+from bank_slip import Bank_Slip
+from payment import Payment
 
 class Bank_Slip_Manager_Tests(unittest.TestCase):
     def test_bank_slip_manager_object_is_created_correctly(self):
@@ -13,6 +15,24 @@ class Bank_Slip_Manager_Tests(unittest.TestCase):
 
         bank_slip_manager = Bank_Slip_Manager(target_invoice)
         self.assertIsInstance(bank_slip_manager, Bank_Slip_Manager)
+
+    def test_register_bank_slip_method(self):
+        now = datetime.datetime.now()
+        invoice = Invoice(now, 5345.12, "Felipe")
+
+        bank_slip_manager = Bank_Slip_Manager(invoice)
+
+        bank_slip = Bank_Slip("001", now, 5345.12)
+
+        bank_slip_manager.register_bank_slip(bank_slip)
+
+        self.assertEqual(len(invoice.payments), 1)
+
+        invoice_payment = invoice.payments[0]
+
+        self.assertEqual(invoice_payment.type, "BOLETO")
+        self.assertEqual(invoice_payment.date, now)
+        self.assertTrue(invoice.is_paid())
 
 
 if __name__ == "__main__":
