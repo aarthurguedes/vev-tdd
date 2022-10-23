f39529d4e6fed5f725db24ca9725135f4e153bee
felipedaf <me@felipedaf.dev>
2022-10-22 22:04:26 -0400

    Adds the instance creation test of invoice

---
+++ src/bank_slip_manager/app/invoice.py
@@ -0,0 +1,5 @@
+class Invoice:
+    def __init__(self, date, value, client_name):
+        self.date = date
+        self.value = value
+        self.client_name = client_name
---
+++ src/bank_slip_manager/tests/invoice_test.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.insert(1, "../app")
+import unittest
+import datetime
+from invoice import Invoice
+
+class Invoice_Test(unittest.TestCase):
+    def test_invoice_object_is_created_correctly(self):
+        random_date = datetime.datetime.now()
+        random_value = 4398.20
+        client_name = "Felipe"
+        print(random_date);
+        curr_invoice = Invoice(random_date, random_value, client_name)
+        self.assertIsInstance(curr_invoice, Invoice)
+
+if __name__ == '__main__':
+    unittest.main()
