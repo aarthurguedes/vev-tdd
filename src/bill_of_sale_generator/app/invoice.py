71f593794ff6ebaf73c4af65f220fde29a5490dd
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2022-10-24 20:10:59 -0300

    Ensure Invoice creation with correct params

--- src/bill_of_sale_generator/app/invoice.py
+++ src/bill_of_sale_generator/app/invoice.py
@@ -1,3 +1,6 @@
 class Invoice:
   def __init__(self, client_name, client_address, service_type, invoice_value):
-    pass
+    self.client_name = client_name
+    self.client_address = client_address
+    self.service_type = service_type
+    self.value = invoice_value
--- src/bill_of_sale_generator/tests/invoice_test.py
+++ src/bill_of_sale_generator/tests/invoice_test.py
@@ -5,3 +5,16 @@ class TestInvoice(unittest.TestCase):
   def test_missing_constructor_attrs(self):
     with self.assertRaises(Exception):
       Invoice()
+  
+  def test_creating(self):
+    client_name = 'Arthur'
+    client_address = '9840 International Dr, Orlando, Florida, US'
+    service_type = 'CONSULTANCY'
+    value = 199.99
+    
+    invoice = Invoice(client_name, client_address, service_type, value)
+    
+    self.assertEqual(invoice.client_name, client_name)
+    self.assertEqual(invoice.client_address, client_address)
+    self.assertEqual(invoice.service_type, service_type)
+    self.assertEqual(invoice.value, value)
