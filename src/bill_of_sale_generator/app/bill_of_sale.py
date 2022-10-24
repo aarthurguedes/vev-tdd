06f2902ddd747c7468dfad59cde474f4f4222e54
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2022-10-24 20:33:19 -0300

    Ensure BillOfSale creation with correct params

--- src/bill_of_sale_generator/app/bill_of_sale.py
+++ src/bill_of_sale_generator/app/bill_of_sale.py
@@ -1,3 +1,5 @@
 class BillOfSale:
   def __init__(self, client_name, value, tax_value):
-    pass
+    self.client_name = client_name
+    self.value = value
+    self.tax_value = tax_value
--- src/bill_of_sale_generator/tests/bill_of_sale_test.py
+++ src/bill_of_sale_generator/tests/bill_of_sale_test.py
@@ -5,3 +5,14 @@ class TestBillOfSale(unittest.TestCase):
   def test_missing_constructor_attrs(self):
     with self.assertRaises(Exception):
       BillOfSale()
+  
+  def test_creating(self):
+    client_name = 'Arthur'
+    value = 199.99
+    tax_value = 49.99
+    
+    bill_of_sale = BillOfSale(client_name, value, tax_value)
+    
+    self.assertEqual(bill_of_sale.client_name, client_name)
+    self.assertEqual(bill_of_sale.value, value)
+    self.assertEqual(bill_of_sale.tax_value, tax_value)
