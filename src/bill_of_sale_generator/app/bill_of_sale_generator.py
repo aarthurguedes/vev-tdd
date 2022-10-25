781052ae8dbf23718ad7c0a40bb72beb028eec89
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2022-10-24 21:54:29 -0300

    Ensure expected tax value

--- src/bill_of_sale_generator/app/bill_of_sale_generator.py
+++ src/bill_of_sale_generator/app/bill_of_sale_generator.py
@@ -4,6 +4,19 @@ class BillOfSaleGenerator():
   def __init__(self):
     pass
 
-  def generate(invoice):
+  def generate(self, invoice):
     if not isinstance(invoice, Invoice):
       raise TypeError('invoice (param) must be Invoice')
+  
+  def calc_tax_value(self, invoice_service_type, invoice_value):
+    match invoice_service_type:
+      case 'CONSULTANCY':
+        tax_rate = 0.25
+        return round(tax_rate * invoice_value, 2)
+      case 'TRAINING':
+        tax_rate = 0.15
+        return round(tax_rate * invoice_value, 2)
+      case _:
+        tax_rate = 0.06
+        return round(tax_rate * invoice_value, 2)
+
--- src/bill_of_sale_generator/tests/bill_of_sale_generator_test.py
+++ src/bill_of_sale_generator/tests/bill_of_sale_generator_test.py
@@ -1,9 +1,21 @@
 import unittest
 
 from app.bill_of_sale_generator import BillOfSaleGenerator
+from app.invoice import Invoice
 
 class TesteBillOfSaleGenerator(unittest.TestCase):
   def test_invalid_param_to_generate(self):
     generator = BillOfSaleGenerator()
     with self.assertRaises(Exception):
       generator.generate('Invoice')
+
+  def test_calc_tax_value(self):
+    invoiceConsultancy = Invoice('Arthur', '9840 International Dr, Orlando, Florida, US', 'CONSULTANCY', 200)
+    invoiceTraining = Invoice('Arthur', '9840 International Dr, Orlando, Florida, US', 'TRAINING', 200)
+    invoiceOther = Invoice('Arthur', '9840 International Dr, Orlando, Florida, US', 'OTHER', 200)
+
+    generator = BillOfSaleGenerator()
+
+    self.assertEqual(generator.calc_tax_value(invoiceConsultancy.service_type, invoiceConsultancy.value), 50)
+    self.assertEqual(generator.calc_tax_value(invoiceTraining.service_type, invoiceTraining.value), 30)
+    self.assertEqual(generator.calc_tax_value(invoiceOther.service_type, invoiceOther.value), 12)
