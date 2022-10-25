4be02b8b9ab419e4ec697f48b6c7622d98958698
Arthur Guedes <arthur.guedes@ccc.ufcg.edu.br>
2022-10-24 21:18:07 -0300

    Ensure to_string returns client name, value and tax value

--- src/bill_of_sale_generator/app/bill_of_sale.py
+++ src/bill_of_sale_generator/app/bill_of_sale.py
@@ -3,3 +3,6 @@ class BillOfSale:
     self.client_name = client_name
     self.value = value
     self.tax_value = tax_value
+
+  def to_string(self):
+    return f'Cliente: {self.client_name} | Valor: {self.value} | Valor do Imposto: {self.tax_value}'
--- src/bill_of_sale_generator/tests/bill_of_sale_test.py
+++ src/bill_of_sale_generator/tests/bill_of_sale_test.py
@@ -16,3 +16,15 @@ class TestBillOfSale(unittest.TestCase):
     self.assertEqual(bill_of_sale.client_name, client_name)
     self.assertEqual(bill_of_sale.value, value)
     self.assertEqual(bill_of_sale.tax_value, tax_value)
+  
+  def test_attrs_in_to_string(self):
+    client_name = 'Arthur'
+    value = 199.99
+    tax_value = 49.99
+    
+    bill_of_sale = BillOfSale(client_name, value, tax_value)
+    container = bill_of_sale.to_string()
+
+    self.assertIn(client_name, container)
+    self.assertIn(str(value), container)
+    self.assertIn(str(tax_value), container)
